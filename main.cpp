35575575fb1095a4dc75616661b1f26ef944cb61
FranUcles <ucles2014@hotmail.com>
2022-04-22 21:05:08 +0200

    Función: Valor Fixes #6

--- main.cpp
+++ main.cpp
@@ -10,7 +10,14 @@ bool Solucion(int nivel, vector<int> &solucion){
 }
 
 int Valor(vector<int> &solucion){
-
+    int ocupados = 0;                                   // Guarda el número de mecánicos con una avería
+    int i = 0;
+    while (i < solucion.size() && solucion[i] != -1) {  // Recorro solo hasta el punto donde no se haya asignado
+        if (solucion[i] > 0)                            // Contabiliza si ese mecánico tiene un trabajo
+            ocupados++;
+        i++;
+    }
+    return ocupados;                                    // Devuelvo los mecánicos ocupados
 }
 
 bool Criterio(int nivel, vector<int> &solucion){
@@ -22,7 +29,7 @@ bool MasHermanos(int nivel, vector<int> &solucion){
 }
 
 void Retroceder(int &nivel, vector<int> &solucion){
-    
+
 }
 
 void Backtracking(vector<int> &solucion){
